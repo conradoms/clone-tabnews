8f1ca60593d45976d974f5909b0a6678febde261
Conrado Silva <conradoms@gmail.com>
2025-10-18 03:02:06 +0000

    Atualiza pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Sisi, voce e o amor da minha vida. Se voce me ama, da uma risadinha.</h1>;
+    return <h1>Oieee. Eu te amo! Muito orgulho de voce!</h1>;
 }
 
 export default Home;
